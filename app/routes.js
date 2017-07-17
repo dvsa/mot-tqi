7674dc3aa2214ce1502b46a14f1d8d2f9864bf2d
Rafal Drewnowski <r.drewnowski@kainos.com>
2017-07-17 17:44:28 +0100

    Added branching for flow with text messages

--- app/routes.js
+++ app/routes.js
@@ -12,4 +12,21 @@ router.get('/testing', function (req, res) {
   res.render('test/test')
 })
 
+router.get('/reminders/v2c/channel', function (req, res) {
+  // get the answer from the query string (eg. ?your-email=false)
+  var selectChannel = req.query.selectChannel
+
+  if (selectChannel == 'email') {
+    // redirect to the relevant page
+    res.redirect('/reminders/v2c/your-email')
+  } else if (selectChannel == 'text') {
+    // redirect to the relevant page
+    res.redirect('/reminders/v2c/your-phone')
+  } else if (selectChannel == 'both') {
+    // redirect to the relevant page
+    res.redirect('/reminders/v2c/your-phone-email')
+  }
+
+})
+
 module.exports = router
